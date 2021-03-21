9a63b920e3fd572bfce3fbd18fa0b572872b2f57
jacob-dumas <jacobdumas@creighton.edu>
2021-03-21 14:23:54 -0500

    Select Customers Form

---
+++ forms/Overview/Overview.js
@@ -0,0 +1,7 @@
+// global variables for database calls
+let req = ""
+let query = ""
+let results = ""
+let pw = "Bailey10"  // ***** put your database password here
+let netID = "jad64177"
+let allCustomerData = []
